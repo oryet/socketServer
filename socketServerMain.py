50f0fd3b6254c24c84ff2bee3aecfa446db3e9ac
jerryGithub <jerry_jzy@126.com>
2019-07-30 16:31:55 +0800

    服务器修改
    增加日志参数

--- socketServerMain.py
+++ socketServerMain.py
@@ -5,16 +5,20 @@ import PublicLib.public as pub
 
 
 if __name__ == "__main__":
+    print("hello world")
+    '''
     pub.loggingConfig('logging.conf')
     defaultSocketConfig = pub.loadDefaultSettings("socket.json")
+
     ip = defaultSocketConfig['ip']
     ipport = defaultSocketConfig['ipport']
     ADDRESS = (ip, ipport)
     logger = logging.getLogger('main')
     logger.info(ADDRESS)
-    t = threading.Thread(target=ss.ServerMonitor, args=(None,))
+    t = threading.Thread(target=ss.ServerMonitor, args=(None,logger))
     t.start()
     ts = threading.Thread(target=ss.SocketSendThread)
     ts.start()
     server = ss.socketserver.ThreadingTCPServer(ADDRESS, ss.Myserver)
     server.serve_forever()
+    '''
